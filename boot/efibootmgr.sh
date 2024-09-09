5cfe4455328a2995bd709acbffa373fc9fff401c
Omar Hany Kasban <omarhanykasban706@gmail.com>
2024-09-10 00:29:25 +0300

    improvements

--- boot/efibootmgr.sh
+++ boot/efibootmgr.sh
@@ -2,5 +2,5 @@
 efibootmgr --create \
  --disk /dev/nvme0n1 --part 1 \
  --label "Arch Linux" \
- --loader /vmlinuz-linux-vfio-lts \
- --unicode 'root=PARTUUID=44bfa8dd-d6d9-4b93-97e1-ec344d274408 rw splash quiet amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction pcie_port_pm=off video=1920x1080 initrd=\initramfs-linux-vfio-lts.img'
+ --loader /vmlinuz-linux-cachyos-lts \
+ --unicode 'root=PARTUUID=44bfa8dd-d6d9-4b93-97e1-ec344d274408 rw splash quiet amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction pcie_port_pm=off video=1920x1080 initrd=\initramfs-linux-cachyos-lts.img'
