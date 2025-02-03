5190bfdf0efcaa45323f86a7dc6a8c8728fb1626
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-02-03 05:48:20 +0200

    boot got updated

---
+++ boot/efibootmgr-lts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+efibootmgr --create \
+ --disk /dev/sdb --part 1 \
+ --label "SteavenLinux LTS" \
+ --loader /vmlinuz-linux-cachyos-lts \
+ --unicode 'root=PARTUUID=348fb182-d4b7-4082-a8ee-04a2e88095b0 rw splash quiet amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction pcie_port_pm=off video=1920x1080 initrd=\initramfs-linux-cachyos-lts.img'
--- boot/efibootmgr.sh
+++ boot/efibootmgr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 efibootmgr --create \
- --disk /dev/nvme0n1 --part 1 \
- --label "Arch Linux" \
- --loader /vmlinuz-linux-cachyos-lts \
- --unicode 'root=PARTUUID=44bfa8dd-d6d9-4b93-97e1-ec344d274408 rw splash quiet amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction pcie_port_pm=off video=1920x1080 initrd=\initramfs-linux-cachyos-lts.img'
+ --disk /dev/sdb --part 1 \
+ --label "SteavenLinux" \
+ --loader /vmlinuz-linux-cachyos \
+ --unicode 'root=PARTUUID=348fb182-d4b7-4082-a8ee-04a2e88095b0 rw splash quiet amd_iommu=on iommu=pt pcie_acs_override=downstream,multifunction pcie_port_pm=off video=1920x1080 initrd=\initramfs-linux-cachyos.img'
